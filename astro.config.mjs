fc64519da4e7b47bdf2eb330c04d14e233af741e
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-04-17 13:15:54 +0200

    Add deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tinaDirective from "./astro-tina-directive/register"
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
+	site: 'https://garanas.github.io',
+	base: 'website-astro-tina',
 	integrations: [mdx(), sitemap(), react(), tinaDirective()],
 });
